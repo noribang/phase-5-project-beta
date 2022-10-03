ea2baa612a96f7baadc1884f08d8ab80f365a289
hoonkim <hoonk9387@gmail.com>
2022-10-03 14:55:03 -0500

    Refactor. Import al child components into App.js. Create separate file for each component.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./components/App";
+import App from "./components/App";
 import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
 // ReactDOM.render(
@@ -15,131 +15,132 @@ import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 //   document.getElementById("root")
 // );
 
-/* Nav styling */
-const linkStyles = {
-  display: "inline-block",
-  width: "50px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
+// /* Nav styling */
+// const linkStyles = {
+//   display: "inline-block",
+//   width: "50px",
+//   padding: "12px",
+//   margin: "0 6px 6px",
+//   background: "blue",
+//   textDecoration: "none",
+//   color: "white",
+// };
 
-/* Navbar */
-function NavBar() {
-  return(
-    <div>
-      <NavLink
-        to="/"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-      to="/about"
-      exact
-      style={linkStyles}
-      activeStyle={{
-        background: "darkblue",
-      }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Login
-      </NavLink>
-      <NavLink
-        to="/signup"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Signup
-      </NavLink>
-      <NavLink
-        to="/messages"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Messages
-      </NavLink>
+// /* Navbar */
+// function NavBar() {
+//   return(
+//     <div>
+//       <NavLink
+//         to="/"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Home
+//       </NavLink>
+//       <NavLink
+//       to="/about"
+//       exact
+//       style={linkStyles}
+//       activeStyle={{
+//         background: "darkblue",
+//       }}
+//       >
+//         About
+//       </NavLink>
+//       <NavLink
+//         to="/login"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Login
+//       </NavLink>
+//       <NavLink
+//         to="/signup"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Signup
+//       </NavLink>
+//       <NavLink
+//         to="/messages"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue",
+//         }}
+//       >
+//         Messages
+//       </NavLink>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-/* Components */
-function Home() {
-  return(
-    <div>
-      <h1>Home route!!!</h1>
-    </div>
-  );
-}
+// /* Components */
+// function Home() {
+//   return(
+//     <div>
+//       <h1>Welcome to Twilio App</h1>
+//     </div>
+//   );
+// }
 
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!!!</h1>
-    </div>
-  );
-}
+// function About() {
+//   return (
+//     <div>
+//       <h1>This is my about component!!!</h1>
+//     </div>
+//   );
+// }
 
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username"/>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password"/>
-        </div>
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  );
-}
+// function Login() {
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username"/>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password"/>
+//         </div>
+//         <input type="submit" value="Submit"/>
+//       </form>
+//     </div>
+//   );
+// }
 
-function Signup() {
-  return(
-    <div>
-      <h1>Sign up here:</h1>
-    </div>
-  );
-}
+// function Signup() {
+//   return(
+//     <div>
+//       <h1>Sign up here:</h1>
+//     </div>
+//   );
+// }
 
-function Messages() {
-  return(
-    <div>
-      <h1>Messages here:</h1>
-    </div>
-  );
-}
+// function Messages() {
+//   return(
+//     <div>
+//       <h1>Messages here:</h1>
+//     </div>
+//   );
+// }
 
 
 /* Routes */
 ReactDOM.render(
   <BrowserRouter>
-    <NavBar />
+    <App />
+    {/* <NavBar />
     <Switch>
       <Route path="/about">
         <About />
@@ -156,7 +157,7 @@ ReactDOM.render(
       <Route exact path="/">
         <Home />
       </Route>
-    </Switch>
+    </Switch> */}
   </BrowserRouter>,
 document.getElementById("root")
 );
