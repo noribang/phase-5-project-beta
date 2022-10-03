50b21041ee55316be38fdf8cac2d70e7ba151df4
hoonkim <hoonk9387@gmail.com>
2022-10-03 14:04:30 -0500

    Add Navbar and routes.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./components/App";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
 // ReactDOM.render(
 //   <BrowserRouter>
@@ -15,6 +15,77 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 //   document.getElementById("root")
 // );
 
+/* Nav styling */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+/* Navbar */
+function NavBar() {
+  return(
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+      to="/about"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: "darkblue",
+      }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/signup"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Signup
+      </NavLink>
+      <NavLink
+        to="/messages"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Messages
+      </NavLink>
+
+    </div>
+  );
+}
+
+/* Components */
 function Home() {
   return(
     <div>
@@ -48,16 +119,39 @@ function Login() {
   );
 }
 
+function Signup() {
+  return(
+    <div>
+      <h1>Sign up here:</h1>
+    </div>
+  );
+}
+
+function Messages() {
+  return(
+    <div>
+      <h1>Messages here:</h1>
+    </div>
+  );
+}
 
 
+/* Routes */
 ReactDOM.render(
   <BrowserRouter>
+    <NavBar />
     <Switch>
       <Route path="/about">
         <About />
       </Route>
       <Route path="/login">
         <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/messages">
+        <Messages />
       </Route>
       <Route exact path="/">
         <Home />
