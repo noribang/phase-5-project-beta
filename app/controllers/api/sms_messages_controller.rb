a8e2130178d957132bbbcfaa048309d06d119a85
hoonkim <hoonk9387@gmail.com>
2022-10-06 14:03:24 -0500

    Uncommented Sms code.

--- app/controllers/api/sms_messages_controller.rb
+++ app/controllers/api/sms_messages_controller.rb
@@ -2,24 +2,24 @@ require 'twilio-ruby'
 
 class Api::SmsMessagesController < ApplicationController
 
-    # # POST /api/sms_messages  api/sms_messages#create
-    # def create
-    #     # Twilio API
-    #     client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+    # POST /api/sms_messages  api/sms_messages#create
+    def create
+        # Twilio API
+        client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
 
-    #     client.messages.create(
-    #         from: ENV['TWILIO_NUMBER'],
-    #         to: sms_message_params['mobile_number'],
-    #         body: sms_message_params['message']
-    #     )
+        client.messages.create(
+            from: ENV['TWILIO_NUMBER'],
+            to: sms_message_params['mobile_number'],
+            body: sms_message_params['message']
+        )
 
-    #     render json: client
-    # end
+        render json: client
+    end
 
-    # private
+    private
 
-    # def sms_message_params
-    #     params.require(:sms_message).permit(:mobile_number, :message)
-    # end
+    def sms_message_params
+        params.require(:sms_message).permit(:mobile_number, :message)
+    end
 
 end
