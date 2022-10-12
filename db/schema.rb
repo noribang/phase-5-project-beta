c20a2a2d5c52bb059926bdd72f383d3d474027ec
hoonkim <hoonk9387@gmail.com>
2022-10-12 17:18:47 -0500

    Add user model, controller and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_020807) do
+ActiveRecord::Schema.define(version: 2022_10_12_200521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_10_06_020807) do
   create_table "sms_messages", force: :cascade do |t|
     t.string "mobile_number"
     t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
