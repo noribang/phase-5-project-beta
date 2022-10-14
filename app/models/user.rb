f33aaf905386476a90b2e1698301c21ef817ddb0
hoonkim <hoonk9387@gmail.com>
2022-10-14 11:36:47 -0500

    Add post /signup, to: users#create to route.rb. Add JSX to Signup component.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    # Bundle install gem 'bcrypt'
+    # Bundle install gem 'bcrypt' in order to 
+    # use macro has_secure_password
     has_secure_password
 
 end
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
     function Signup() {
+        // State
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
+        const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+        // Handles onSubmit event.
+        function handleSubmit() {
+
+        }
+
+        // View JSX
         return(
-        <div>
-            <h1>Sign up here:</h1>
-        </div>
+            <>
+                <h1>Sign up</h1>
+                <form onSubmit={handleSubmit}>
+                   <div>
+                        <label htmlFor="username">Username:</label>
+                        <input 
+                            type='text'
+                            id='username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                   </div>
+                   <div>
+                        <label htmlFor="password">Password:</label>
+                        <input 
+                            type='password'
+                            id='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                   </div>
+                   <div>
+                        <label htmlFor="password_confirmation">Confirm Password:</label>
+                        <input 
+                            type='password'
+                            id='password_confirmation'
+                            value={passwordConfirmation}
+                            onChange={(e) => setPasswordConfirmation(e.target.value)}
+                        />
+                   </div> 
+                    <button type="submit">Submit</button>
+                </form>
+            
+            </>
         );
     }
   
